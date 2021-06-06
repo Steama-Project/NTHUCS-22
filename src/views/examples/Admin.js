29c62fb70292f26a58c8e400aed364def5c87343
Muzi-P <phiwokuhlegondwe@gmail.com>
2021-06-06 13:35:47 +0800

    feat: question report download

--- src/views/examples/Admin.js
+++ src/views/examples/Admin.js
@@ -19,48 +19,65 @@ import React from "react";
 // react plugin used to create charts
 
 // reactstrap components
-import {
-  Row,
-  Col,
-  Container,
-  Button,
-} from "reactstrap";
-import AdminTab from '../IndexSections/AdminTab';
+import { Row, Col, Container, Button } from "reactstrap";
+import AdminTab from "../IndexSections/AdminTab";
+import { selectCurrentUser } from "views/redux/user/user-selector";
+import { useSelector } from "react-redux";
 
 // core components
 import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
 
+import FileDownload from "js-file-download";
+import axios from "axios";
+
 export default function LandingPage() {
+  const currentUser = useSelector((state) => selectCurrentUser(state));
+  const { token } = currentUser;
+
   React.useEffect(() => {
     document.body.classList.toggle("landing-page");
     // Specify how to clean up after this effect:
     return function cleanup() {
       document.body.classList.toggle("landing-page");
     };
-  },[]);
+  }, []);
+
+  const handleGenerateSchedule = () => {
+    axios({
+      url: "http://localhost:3001/question/download-report",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method: "GET",
+      responseType: "blob", // Important
+    }).then((response) => {
+      FileDownload(response.data, "question_report.xlsx");
+    });
+  };
   return (
     <>
       <ExamplesNavbar />
-      
+
       <div className="wrapper section section-tabs">
-      <Container>
-        <div className="content-center">
-            <Row className="row-grid justify-content-between align-items-center text-left ml-auto mr-auto"> 
-              <Col lg="12" md="0" >
+        <Container>
+          <div className="content-center">
+            <Row className="row-grid justify-content-between align-items-center text-left ml-auto mr-auto">
+              <Col lg="12" md="0">
                 {/* <Tabs/> */}
-                <AdminTab/>
+                <AdminTab />
                 <Button
                   className="nav-link d-lg-block"
                   color="success"
                   size="lg"
-                  style={{margin: "20px"}}
-                  >
-                   Download Excel Sheet
-                   </Button>
+                  style={{ margin: "20px" }}
+                  onClick={handleGenerateSchedule}
+                >
+                  Download Excel Sheet
+                </Button>
               </Col>
             </Row>
-          </div>  
-        </Container> 
+          </div>
+        </Container>
       </div>
     </>
   );
